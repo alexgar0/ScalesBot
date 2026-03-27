9c04f01050b926c23b31dfa90de96df9c820e1a0
shellcode <example@gmail.com>
2026-03-27 03:42:41 +0300

    feat: add agent system prompt

--- src/core/agent.py
+++ src/core/agent.py
@@ -6,6 +6,13 @@ from pydantic_ai.models.openai import OpenAIChatModel
 from core.config import settings
 from core.deps import AgentDependencies
 
+def get_system_prompt() -> str:
+    system_prompt = settings.workflow_path / "AGENT.MD"
+    if system_prompt.exists():
+        return system_prompt.read_text()
+    else:
+        return "You're a helpful assistant"
+
 agent = Agent(
     OpenAIChatModel(
         "",
@@ -14,5 +21,6 @@ agent = Agent(
             temperature=settings.temperature, max_tokens=settings.context_window
         ),
     ),
+    system_prompt=get_system_prompt(),
     deps_type=AgentDependencies,
 )
